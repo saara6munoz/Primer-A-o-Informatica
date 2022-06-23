3bf01ade5193ac7f4d75a267443251ae942a248a
saara6munoz <97033988+saara6munoz@users.noreply.github.com>
2022-06-23 17:40:46 -0400

    Add files via upload

---
+++ recursion.py
@@ -0,0 +1,28 @@
+# coding: utf-8
+# Your code here!
+
+def sumatoria(n):
+    if n == 1:
+        return 1
+    else:
+        return n + sumatoria(n-1)
+        
+def sumatoria2(acum,i,n):
+    if i == n:
+        return acum + n
+    else:
+        return sumatoria2(acum + i, i+1, n)
+
+def sumatoria3(acum,n):
+    if n == 1:
+        return acum + n
+    else:
+        return sumatoria3(acum+n, n-1)
+
+suma1 = sumatoria(10)
+suma2 = sumatoria2(0,1,10)
+suma3 = sumatoria3(0,10)
+
+print("suma1: ", suma1)
+print("suma2: ", suma2)
+print("suma3: ", suma3)
